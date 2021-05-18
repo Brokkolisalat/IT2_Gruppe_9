9961486e416e42571fcffcc78267efe49a796b28
Brokkolisalat <72988046+Brokkolisalat@users.noreply.github.com>
2021-05-18 15:08:36 +0200

    fix

--- main.js
+++ main.js
@@ -3,22 +3,32 @@ function showData(daten, filter) {
 	// Anzeige des Filters
 	let filter_element = d3.select('#filter');
 	valid_filter = isValidFilter(filter, daten);
-	if(valid_filter) filter_element.text("Filter: " + filter);
-	else filter_element.text("Filter ungültig: " + filter);
-
+	if(valid_filter){ 
+	filter_element.text("Filter: " + filter);
     //Rückgabe der d3.selectAll - Methode in variable p speichern.(Alle Kindelemente von content, die p- Elemente sind.) Am Anfang gibt es noch keine.
     var list = d3.select("#list").selectAll("ul").data(daten);
     //.enter().append(): Daten hinzufuegen falls es mehr Daten als Elemente im HTML gibt.
     //geschieht hier für jede Zeile von daten.
     list.enter().append("li")
         .text(function (daten) {
-        	if(valid_filter)
-            return "Schlüssel: " + filter + "Wert: " + daten.werte[filter] + "Uhrzeit: " + daten.datum;
-            else
-            return "Schlüssel: " + d3.keys(daten.werte) + "Wert: " + daten.werte + "Uhrzeit: " + daten.datum;
+            return "Schlüssel: " + filter + ", Wert: " + daten.werte[filter] + ", Uhrzeit: " + daten.datum;
         });
     //.exit().remove(): Daten löschen, falls es mehr Elemente im HTML als Daten gibt.
     list.exit().remove();
+    }
+    else{
+    filter_element.text("Filter ungültig: " + filter);
+    d3.select('#valid_filters').text( " Gültige Filter: ");
+    d3.select("#list").selectAll("ul").data(d3.keys(daten["0"].werte)).enter().append("li")
+        .text(function (daten) {
+            return daten;
+        });
+    /*for(i = 0; i < d3.keys(daten["0"].werte).length; i++){
+    	d3.select("#list").select("ul").enter().append("li")
+        .text(d3.keys(daten["0"].werte);
+    }*/
+    }
+    
 }
 
 function callData(datenEmpfangen,error, filter) {
@@ -31,7 +41,7 @@ function callData(datenEmpfangen,error, filter) {
 
 // Einstiegspunkt
 function getData() {
-   var filter = "Ampel rot"
+   var filter = "Ampel roat"
     d3.json("https://it2wi1.if-lab.de/rest/ft_ablauf").then(function (data, error) {
         callData(data, error, filter)
     });
@@ -44,51 +54,3 @@ function isValidFilter(filter, daten){
 	}
 	return false;
 }
-
-/*
-SCHLÜSSELWERTE IN JSON:
-H-vertikal
-H-horizontal
-V-vertikal	
-V-drehen	
-V-horizontal
-B-Referenzschalter Drehkranz (Pos. Sauger)	" false"
-B-Referenzschalter Drehkranz (Pos. Foerderband)	" false"
-B-Lichtschranke Ende Foerderband	" true"
-B-Referenzschalter Drehkranz (Pos. Saege)	" false"
-B-Referenzschalter Sauger (Pos. Drehkranz)	" true"
-B-Referenzschalter Ofenschieber Innen	" false"
-B-Referenzschalter Ofenschieber Aussen	" true"
-B-Referenzschalter Sauger (Pos. Brennofen)	" false"
-B-Lichtschranke Brennofen	" true"
-S-Lichtschranke Eingang	" true"
-S-Lichtschranke nach Farbsensor	" true"
-S-Lichtschranke weiss	" true"
-S-Lichtschranke rot	" true"
-S-Lichtschranke blau	" true"
-Umsetzer Endanschlag 1 (3B1)	" false"
-Umsetzer Endanschlag 2 (3B2)	" false"
-Referenztaster horizontal	" true"
-Lichtschranke innen	" true"
-Lichtschranke aussen	" true"
-Referenztaster vertikal	" true"
-Referenztaster Ausleger vorne	" false"
-Referenztaster Ausleger hinten	" true"
-V-Referenzschalter vertikal	" true"
-V-Referenzschalter horizontal	" true"
-V-Referenzschalter drehen	" true"
-B-Motor Drehkranz im Uhrzeigersinn	" false"
-B-Motor Drehkranz gegen Uhrzeigersinn	" false"
-B-Motor Foerderband vorwaerts	" false"
-B-Motor Saege	" false"
-B-Motor Ofenschieber Einfahren	" false"
-B-Motor Ofenschieber Ausfahren	" false"
-B-Motor Sauger zum Ofen	" false"
-B-Motor Sauger zum Drehkranz	" false"
-B-Leuchte Ofen	" false"
-S-Motor Foerderband	" false"
-Ampel rot	" false"
-Ampel orange	" false"
-Ampel gruen	" false"
-Ampel weiss
-*/
