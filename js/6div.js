579a344e76ab1d6d0d2ce7456230b74ea2f8ff89
mttno1 <thethanh135@gmail.com>
2021-06-12 19:58:30 +0200

    commit

---
+++ js/6div.js
@@ -0,0 +1,22 @@
+$(function () {
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+});
+document.getElementById("infor-sensor-1").onclick = function (){
+    location.href = 'Laserschranke_Information.html';
+}
+document.getElementById("infor-sensor-2").onclick = function (){
+    location.href = 'Laserschranke_Information.html';
+}
+document.getElementById("infor-sensor-3").onclick = function (){
+    location.href = 'Laserschranke_Information.html';
+}
+document.getElementById("infor-sensor-4").onclick = function (){
+    location.href = 'Laserschranke_Information.html';
+}
+document.getElementById("infor-sensor-5").onclick = function (){
+    location.href = 'Laserschranke_Information.html';
+}
+document.getElementById("infor-sensor-6").onclick = function (){
+    location.href = 'Laserschranke_Information.html';
+}
---
+++ js/option.js
@@ -0,0 +1,11 @@
+$(function () {
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+});
+
+document.getElementById("sensor").onclick = function (){
+    location.href = '6div.html';
+}
+document.getElementById("idontknow").onclick = function (){
+    location.href = '6div.html';
+}
