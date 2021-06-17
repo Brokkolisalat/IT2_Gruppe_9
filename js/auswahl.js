aa1c8f6a0d69e7be26764ea0abd8becb28c59b13
Brokkolisalat <72988046+Brokkolisalat@users.noreply.github.com>
2021-06-17 13:02:22 +0200

    fix

--- js/auswahl.js
+++ js/auswahl.js
@@ -2,21 +2,39 @@ $(function () {
     $("#header").load("header.html");
     $("#footer").load("footer.html");
 });
+
+// Auswahl bei Sensorik 
 document.getElementById("infor-sensor-1").onclick = function (){
-    location.href = 'Hochregallager_Information.html';
+    location.href = 'Hochregallager_Sensorik.html';
 }
 document.getElementById("infor-sensor-2").onclick = function (){
-    location.href = 'Hochregallager_Information.html';
+    location.href = 'Hochregallager_Sensorik.html';
 }
 document.getElementById("infor-sensor-3").onclick = function (){
-    location.href = 'Hochregallager_Information.html';
+    location.href = 'Hochregallager_Sensorik.html';
 }
 document.getElementById("infor-sensor-4").onclick = function (){
-    location.href = 'Hochregallager_Information.html';
+    location.href = 'Hochregallager_Sensorik.html';
 }
 document.getElementById("infor-sensor-5").onclick = function (){
-    location.href = 'Hochregallager_Information.html';
+    location.href = 'Hochregallager_Sensorik.html';
 }
 document.getElementById("infor-sensor-6").onclick = function (){
-    location.href = 'Hochregallager_Information.html';
+    location.href = 'Hochregallager_Sensorik.html';
+}
+// Auswahl bei Wegstrecke
+document.getElementById("infor-wege-1").onclick = function (){
+    location.href = 'Sortierstrecke_Wege.html';
+}
+document.getElementById("infor-wege-2").onclick = function (){
+    location.href = 'Sortierstrecke_Wege.html';
+}
+document.getElementById("infor-wege-3").onclick = function (){
+    location.href = 'Sortierstrecke_Wege.html';
+}
+document.getElementById("infor-wege-4").onclick = function (){
+    location.href = 'Sortierstrecke_Wege.html';
+}
+document.getElementById("infor-wege-6").onclick = function (){
+    location.href = 'Sortierstrecke_Wege.html';
 }
--- js/option.js
+++ js/option.js
@@ -7,14 +7,5 @@ document.getElementById("sensor").onclick = function (){
     location.href = 'auswahl_sensorik.html';
 }
 document.getElementById("wege").onclick = function (){
-    location.href = 'auswahL_wege.html';
-}
-
-// TODO: Übergabe aktueller Sensor, damit aufgerufende HTML-Page nur einmal erstellt werden muss!
-document.getElementById("diagram").onclick = function (){
-    location.href = 'diagramm.html';
-}
-// TODO: Übergabe aktueller Sensor, damit aufgerufende HTML-Page nur einmal erstellt werden muss!
-document.getElementById("history").onclick = function (){
-    location.href = 'historie.html';
+    location.href = 'auswahl_wege.html';
 }
