5ce445f96ccfcd066ca7675d8d279042c9d2b432
Brokkolisalat <72988046+Brokkolisalat@users.noreply.github.com>
2021-06-19 11:55:24 +0200

    fix

--- js/auswahl_anlage.js
+++ js/auswahl_anlage.js
@@ -7,24 +7,24 @@ var anlage_sensor_loc = "auswahl_darstellung.html";
 
 // Auswahl bei Sensorik 
 document.getElementById("infor-sensor-1").onclick = function (){
+	setCurrentAnlage(1);
     location.href = anlage_sensor_loc;
-    setCurrentAnlage(1);
 }
 document.getElementById("infor-sensor-2").onclick = function (){
+	setCurrentAnlage(2);
     location.href = anlage_sensor_loc;
-    setCurrentAnlage(2);
 }
 document.getElementById("infor-sensor-3").onclick = function (){
+	setCurrentAnlage(3);
     location.href = anlage_sensor_loc;
-    setCurrentAnlage(3);
 }
 document.getElementById("infor-sensor-4").onclick = function (){
-    location.href = anlage_sensor_loc;
     setCurrentAnlage(4);
+    location.href = anlage_sensor_loc;
 }
 document.getElementById("infor-sensor-5").onclick = function (){
-    location.href = anlage_sensor_loc;
     setCurrentAnlage(5);
+    location.href = anlage_sensor_loc;
 }
 document.getElementById("infor-sensor-6").onclick = function (){
     location.href = anlage_sensor_loc;
