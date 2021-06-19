b73912debdae9c2a097fb461237ad485e8626384
Brokkolisalat <72988046+Brokkolisalat@users.noreply.github.com>
2021-06-19 13:08:14 +0200

    dynamischer Headertext

--- js/visualize.js
+++ js/visualize.js
@@ -8,17 +8,18 @@ var dummy_bis = "15.05.2019 13:00:00";
 
 function visualizeDiagram(){
 	var anlage = getCurrentAnlageText();
+	d3.select("#head_text").text('Zusammenfassung ' + anlage);
 	getData(anlage, dummy_von, dummy_bis);
 }
 
 function visualizeHistory(){
 	var anlage = getCurrentAnlageText();
-
+	d3.select("#head_text").text('Historie ' + anlage);
 	getData(anlage, dummy_von, dummy_bis);
 }
 
 function visualizeWege(){
 	var anlage = getCurrentAnlageText();
-
+	d3.select("#head_text").text('Zurückgelegte Wegstrecke ' + anlage);
 	getData(anlage, dummy_von, dummy_bis);
 }
