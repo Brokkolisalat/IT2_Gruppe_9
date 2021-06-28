04cd3bbe7696994f4e81157d1e07adefaa92c13a
mttno1 <thethanh135@gmail.com>
2021-06-28 16:14:10 +0200

    Zeitauswahl erstellen, CSS anpassen

---
+++ js/Time.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+});
+
+$( document ).ready(function() {
+	$('#datetimepicker1').datetimepicker();
+    $('#datetimepicker2').datetimepicker();
+});
