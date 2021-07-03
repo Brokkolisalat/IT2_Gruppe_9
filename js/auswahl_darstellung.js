55e5ac10a7a0595b097bf2e61c05515f9664f420
Brokkolisalat <72988046+Brokkolisalat@users.noreply.github.com>
2021-07-03 20:19:18 +0200

    alten history code deaktiviert

--- js/auswahl_darstellung.js
+++ js/auswahl_darstellung.js
@@ -12,9 +12,9 @@ function onLoad(){
 document.getElementById("diagram").onclick = function (){
     location.href = 'diagram.html';
 }
-document.getElementById("history").onclick = function (){
+/*document.getElementById("history").onclick = function (){
     location.href = 'history.html';
-}
+}*/
 document.getElementById("wege").onclick = function (){
     location.href = 'wege.html';
 }
